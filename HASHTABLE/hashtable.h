6e5fc62f03ef8e9aef74ece3d8a1957f861fda49
Jeecoo <johnmark.econar7@gmail.com>
2024-05-24 18:10:02 +0800

    hashtable polyquad

---
+++ HASHTABLE/hashtable.h
@@ -0,0 +1,106 @@
+#include <string>
+
+using namespace std;
+
+class HashTable {
+private:
+    string* table;
+    int N;
+    int count;
+    string DELETED;
+
+
+    unsigned long long polynomialHash(const string& key) const {
+        unsigned long long hash = 0;
+        unsigned long long a = 7;
+        for (char ch : key) {
+            hash = hash * a + (ch - 'a' + 1);
+        }
+        return hash;
+    }
+
+    // MAD compression function with a = 11, b = 251, c = 319
+    int compress(unsigned long long hash) const {
+        unsigned long long a = 11;
+        unsigned long long b = 251;
+        unsigned long long c = 319;
+        return (a * hash + b) % c;
+    }
+
+public:
+    HashTable(int capacity) : N(capacity), count(0), DELETED("<DELETED>") {
+        table = new string[N];
+        for (int i = 0; i < N; ++i) {
+            table[i] = "";
+        }
+    }
+
+    ~HashTable() {
+        delete[] table;
+    }
+
+    // Insert using quadratic probing
+    void insert(const string& key) {
+        unsigned long long hash = polynomialHash(key);
+        int index = compress(hash);
+        int i = 0;
+        while (!table[(index + i * i) % N].empty() && table[(index + i * i) % N] != DELETED) {
+            i++;
+        }
+        table[(index + i * i) % N] = key;
+        count++;
+    }
+
+    // search using quadratic probing
+    int search(const string& key) const {
+        unsigned long long hash = polynomialHash(key);
+        int index = compress(hash);
+        int i = 0;
+        while (!table[(index + i * i) % N].empty()) {
+            if (table[(index + i * i) % N] == key) {
+                return (index + i * i) % N; // Return the index where the key is found
+            }
+            i++;
+            if (i == N) {
+                break;
+            }
+        }
+        return -1; // Key not found
+    }
+
+    // Remove using quadratic probing
+    void remove(const string& key) {
+        unsigned long long hash = polynomialHash(key);
+        int index = compress(hash);
+        int i = 0;
+        while (!table[(index + i * i) % N].empty()) {
+            if (table[(index + i * i) % N] == key) {
+                table[(index + i * i) % N] = DELETED;
+                count--;
+                return;
+            }
+            i++;
+            if (i == N) {
+                break;
+            }
+        }
+    }
+
+    void print() const {
+        for (int i = 0; i < N; ++i) {
+            if (!table[i].empty() && table[i] != DELETED) {
+                cout << "[" << i << "]: " << table[i] << endl;
+            }
+        }
+    }
+
+//    // Get the number of elements in the hash table
+//    int size() const {
+//        return count;
+//    }
+//
+//    // Get the capacity of the hash table
+//    int capacity() const {
+//        return N;
+//    }
+};
---
+++ HASHTABLE/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "hashtable.h"
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the capacity of N: ";
+    cin >> n;
+
+    HashTable *table = new HashTable(n);
+
+    char op;
+    string key;
+
+    do {
+        cout << "Operation: ";
+        cin >> op;
+        switch(op) {
+            case 'i':
+                cin >> key;
+                table->insert(key);
+                break;
+            case 's':
+                cin >> key;
+                cout << table->search(key);
+                cout << "\n";
+                break;
+            case 'r':
+                cin >> key;
+                table->remove(key);
+                break;
+            case 'p':
+                table->print();
+                break;
+            case 'x':
+                cout << "Exiting...";
+                break;
+            default:
+                cout << "Invalid" << endl;
+        }
+    } while(op != 'x');
+
+    delete table;
+
+    return 0;
+}
